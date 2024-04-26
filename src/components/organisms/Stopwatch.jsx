bd7d87f4b9e9cb2066e5610882851bb9e0678c41
KenJamesHioki <kenj.hioki@gmail.com>
2024-04-26 13:35:24 +0900

    [add]スタート、ストップ、リセットの基本機能を実装
    最低限の機能のみを実装。

--- src/components/organisms/Stopwatch.jsx
+++ src/components/organisms/Stopwatch.jsx
@@ -5,42 +5,61 @@ import { ActionButtons } from "../molecules/ActionButtons";
 import { useEffect, useRef, useState } from "react";
 
 export const Stopwatch = () => {
+  //TODO:stateが無駄に多い気がする。減らせないかを検討。
   const [isRunning, setIsRunning] = useState(false);
-  const [accumulatedTime, setAccumulatedTime] = useState("00:00.000");
+  const [accumulatedTime, setAccumulatedTime] = useState(0);
+  const [displayedTime, setDisplayedTime] = useState("00:00.000");
   const [startTime, setStartTime] = useState(null);
   const intervalIdRef = useRef(null);
 
-  useEffect(()=>{
+  useEffect(() => {
     if (!isRunning) return;
+    let elapseTimeMs = 0;
     intervalIdRef.current = setInterval(() => {
-      alert('stopwatch')
-    }, 1000);
+      elapseTimeMs = Date.now() - startTime + accumulatedTime;
+      const minutes = Math.floor(elapseTimeMs / (1000 * 60));
+      const seconds = Math.floor((elapseTimeMs % (1000 * 60)) / 1000);
+      const milliseconds = elapseTimeMs % 1000;
+      const nextDisplayedTime = `
+      ${minutes.toString().padStart(2, "0")}:
+      ${seconds.toString().padStart(2, "0")}.
+      ${milliseconds.toString().padStart(3, "0")}`;
+      setDisplayedTime(nextDisplayedTime);
+    }, 10);
 
-    return ()=>clearInterval(intervalIdRef.current);
+    return () => {
+      clearInterval(intervalIdRef.current);
+      setAccumulatedTime(elapseTimeMs);
+    };
 
-  },[isRunning])
+  }, [isRunning]);
 
-  const handleStart =()=>{
+  const handleStart = () => {
     setIsRunning(true);
     setStartTime(Date.now());
-  }
+  };
 
-  const handleStop =()=>{
+  const handleStop = () => {
     setIsRunning(false);
-  }
+  };
 
-  const handleReset =()=>{
-    setAccumulatedTime("reset!");
-  }
+  const handleReset = () => {
+    setAccumulatedTime(0);
+    setDisplayedTime("00:00.000");
+  };
 
   return (
     <SWrapper>
       <Card backgroundColor="#eee" border="1px #ddd solid">
         <Title>STOPWATCH</Title>
         <Card backgroundColor="#fff" border="1px #ddd solid">
-          {accumulatedTime}
+          {displayedTime}
         </Card>
-        <ActionButtons handleStart={handleStart} handleStop={handleStop} handleReset={handleReset}/>
+        <ActionButtons
+          handleStart={handleStart}
+          handleStop={handleStop}
+          handleReset={handleReset}
+        />
       </Card>
     </SWrapper>
   );
