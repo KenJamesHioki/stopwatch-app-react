87e71a672b74cb0f07512134993240aba8c75d72
KenJamesHioki <kenj.hioki@gmail.com>
2024-04-26 13:17:15 +0900

    [add]各ボタンの挙動を追加
    各種ボタンのonClickイベントを追加

--- src/components/molecules/ActionButtons.jsx
+++ src/components/molecules/ActionButtons.jsx
@@ -1,12 +1,12 @@
 import styled from "@emotion/styled";
 import { Button } from "../atoms/Button";
 
-export const ActionButtons =({onClick,children})=>{
+export const ActionButtons =({handleStart, handleStop, handleReset})=>{
   return (
     <SWrapper>
-      <Button backgroundColor="#5ced73">START</Button>
-      <Button backgroundColor="#ff8f8f">STOP</Button>
-      <Button backgroundColor="#a3a3ff">RESET</Button>
+      <Button backgroundColor="#5ced73" onClick={handleStart}>START</Button>
+      <Button backgroundColor="#ff8f8f" onClick={handleStop}>STOP</Button>
+      <Button backgroundColor="#a3a3ff" onClick={handleReset}>RESET</Button>
     </SWrapper>
   );
 }
--- src/components/organisms/Stopwatch.jsx
+++ src/components/organisms/Stopwatch.jsx
@@ -2,14 +2,45 @@ import styled from "@emotion/styled";
 import { Card } from "../atoms/Card";
 import { Title } from "../atoms/Title";
 import { ActionButtons } from "../molecules/ActionButtons";
+import { useEffect, useRef, useState } from "react";
 
 export const Stopwatch = () => {
+  const [isRunning, setIsRunning] = useState(false);
+  const [accumulatedTime, setAccumulatedTime] = useState("00:00.000");
+  const [startTime, setStartTime] = useState(null);
+  const intervalIdRef = useRef(null);
+
+  useEffect(()=>{
+    if (!isRunning) return;
+    intervalIdRef.current = setInterval(() => {
+      alert('stopwatch')
+    }, 1000);
+
+    return ()=>clearInterval(intervalIdRef.current);
+
+  },[isRunning])
+
+  const handleStart =()=>{
+    setIsRunning(true);
+    setStartTime(Date.now());
+  }
+
+  const handleStop =()=>{
+    setIsRunning(false);
+  }
+
+  const handleReset =()=>{
+    setAccumulatedTime("reset!");
+  }
+
   return (
     <SWrapper>
       <Card backgroundColor="#eee" border="1px #ddd solid">
         <Title>STOPWATCH</Title>
-        <Card backgroundColor="#fff" border="1px #ddd solid">00:00.000</Card>
-        <ActionButtons />
+        <Card backgroundColor="#fff" border="1px #ddd solid">
+          {accumulatedTime}
+        </Card>
+        <ActionButtons handleStart={handleStart} handleStop={handleStop} handleReset={handleReset}/>
       </Card>
     </SWrapper>
   );
