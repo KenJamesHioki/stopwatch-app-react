5fbd69b96fb6c823e3221dcce81bdfd040649beb
KenJamesHioki <kenj.hioki@gmail.com>
2024-04-30 11:58:44 +0900

    [update]stateで管理する変数を減らした
    stateで管理する変数はisRunningとelapseTimeのみに変更

--- src/components/organisms/Stopwatch.jsx
+++ src/components/organisms/Stopwatch.jsx
@@ -5,17 +5,15 @@ import { ActionButtons } from "../molecules/ActionButtons";
 import { useCallback, useEffect, useRef, useState } from "react";
 
 export const Stopwatch = () => {
-  //TODO:stateが無駄に多い気がする。減らせないかを検討。
   const [isRunning, setIsRunning] = useState(false);
-  const [accumulatedTime, setAccumulatedTime] = useState(0);
-  const [displayedTime, setDisplayedTime] = useState("00:00.000");
+  const [elapseTime, setElapseTime] = useState(0);
   const intervalIdRef = useRef(null);
   const startTimeRef = useRef(null);
 
-  const convertToDisplayedTime = (elapseTimeMs) => {
-    const minutes = Math.floor(elapseTimeMs / (1000 * 60));
-    const seconds = Math.floor((elapseTimeMs % (1000 * 60)) / 1000);
-    const milliseconds = elapseTimeMs % 1000;
+  const convertToDisplayedTime = (elapseTime) => {
+    const minutes = Math.floor(elapseTime / (1000 * 60));
+    const seconds = Math.floor((elapseTime % (1000 * 60)) / 1000);
+    const milliseconds = elapseTime % 1000;
     return `
     ${minutes.toString().padStart(2, "0")}:${seconds
       .toString()
@@ -25,16 +23,12 @@ export const Stopwatch = () => {
 
   useEffect(() => {
     if (!isRunning) return;
-    let elapseTimeMs = 0;
     intervalIdRef.current = setInterval(() => {
-      elapseTimeMs = Date.now() - startTimeRef.current + accumulatedTime;
-      const nextDisplayedTime = convertToDisplayedTime(elapseTimeMs);
-      setDisplayedTime(nextDisplayedTime);
+      setElapseTime(Date.now() - startTimeRef.current + elapseTime);
     }, 10);
 
     return () => {
       clearInterval(intervalIdRef.current);
-      setAccumulatedTime(elapseTimeMs);
     };
   }, [isRunning]);
 
@@ -48,8 +42,7 @@ export const Stopwatch = () => {
   }, []);
 
   const handleReset = useCallback(() => {
-    setAccumulatedTime(0);
-    setDisplayedTime("00:00.000");
+    setElapseTime(0);
   }, []);
 
   return (
@@ -57,7 +50,7 @@ export const Stopwatch = () => {
       <SAppCard backgroundColor="#eee" border="1px #ddd solid">
         <STitle>STOPWATCH</STitle>
         <SStopwatchCard backgroundColor="#fff" border="1px #ddd solid">
-          {displayedTime}
+          {convertToDisplayedTime(elapseTime)}
         </SStopwatchCard>
         <ActionButtons
           handleStart={handleStart}
